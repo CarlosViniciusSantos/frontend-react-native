4c9b2518a54aacd32d0a8efc8304b705bb8abdd7
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-08-29 17:45:50 -0300

    get accounts from api

--- components/Content.js
+++ components/Content.js
@@ -1,52 +1,45 @@
+import { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from "react-native";
 // import CardAccount from "./CardAccount";
 import NewCardAccount from "./NewCardAccount";
 
 
-
 export default function Content() {
-    const acocunts = [
-        {
-            id: 1,
-            service: "Gmail",
-            imgUrl: "https://static.vecteezy.com/system/resources/previews/022/484/516/original/google-mail-gmail-icon-logo-symbol-free-png.png",
-            userName: "carlos.vinicius1@aluno.ifsp.edu.br"
-        },
-        {
-            id: 2,
-            service: "Instragram",
-            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/768px-Instagram_icon.png",
-            userName: "c.ss_pv"
-        },
-        {
-            id: 3,
-            service: "GitHub",
-            imgUrl: "https://as2.ftcdn.net/v2/jpg/02/50/30/59/1000_F_250305943_sDC6la1N1fDl3bLgfLxOkQwItIodsdMb.jpg",
-            userName: "CarlosViniciusSantos"
-        },
-        {
-            id: 4,
-            service: "Facebook",
-            imgUrl: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/facebook-logo-design-template-14a38c75b25fe8a11e30a7202e6846cc_screen.jpg?ts=1616956224",
-            userName: "Carlos Vinicius"
+    const [accounts, setAccounts] = useState([])
+
+    useEffect(() => {
+        const getAccounts = async () => {
+            const response = await fetch('http://localhost:3000/account/list')
+            if (response.ok) {
+                const data = await response.json()
+                console.log(data)
+                setAccounts(data.accounts)
+                return
+            }
+            console.log('Erro ao carregar accounts')
+            return
         }
-    ]
+
+        getAccounts()
+    }, [])
+
 
     return (
         <View style={styles.content}>
+
+            {accounts.length === 0 && <Text>Loading...</Text>}
+
             {
-                acocunts.map((acocunt) =>
+                accounts.map((account) =>
                     <NewCardAccount
-                        key={acocunt.id}
-                        service={acocunt.service}
-                        imgUrl={acocunt.imgUrl}
-                        userName={acocunt.userName}
+                        key={account.id}
+                        service={account.service}
+                        imgUrl={account.logo_image}
+                        userName={account.username}
                     />
                 )
             }
-
         </View>
-
     )
 }
 
