1d4cce6b4a7face8e6e2663718a98c45138e898e
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-09-12 17:25:55 -0300

    add create and delete account

--- components/Content.js
+++ components/Content.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react'
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, Pressable} from "react-native";
 // import CardAccount from "./CardAccount";
 import NewCardAccount from "./NewCardAccount";
 
 
 export default function Content() {
     const [accounts, setAccounts] = useState([])
+    const [txtServico, setTxtServico] = useState('')
+    const [txtUsername, setTxtUsername] = useState('')
+    const [txtPass, setTxtPass] = useState('')
+    const [txtImgUrl, setTxtImgUrl] = useState('')
 
     useEffect(() => {
         const getAccounts = async () => {
@@ -23,9 +27,70 @@ export default function Content() {
         getAccounts()
     }, [])
 
+    const handlerCreateAccount =async () =>{
+        const account = {
+            service: txtServico,
+            username: txtUsername,
+            logo_image: txtImgUrl,
+            pass: txtPass,
+            user_id:1
+        }
 
+        const response = await fetch('http://localhost:3000/account',{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(account)
+        })
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data)
+            setAccounts([data.account,...accounts])
+            return
+        }
+        console.log('Erro ao carregar accounts')
+        return
+
+    }
     return (
         <View style={styles.content}>
+
+            <View>
+                <Text>Serviço:</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setTxtServico}
+                    value={txtServico}
+                    placeholder='Digite o nome do serviço...'
+                    placeholderTextColor='gray'
+                />
+                <Text>Username:</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setTxtUsername}
+                    value={txtUsername}
+                />
+                <Text>Password:</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setTxtPass}
+                    value={txtPass}
+                />
+                <Text>Url da Imagem:</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setTxtImgUrl}
+                    value={txtImgUrl}
+                    keyboardType='url'
+                />
+                <Pressable
+                style={styles.button}
+                onPress={handlerCreateAccount}          
+                >
+                    <Text style={styles.textCad}>Cadastrar</Text>
+                </Pressable>
+            </View>
 
             {accounts.length === 0 && <Text>Loading...</Text>}
 
@@ -33,9 +98,12 @@ export default function Content() {
                 accounts.map((account) =>
                     <NewCardAccount
                         key={account.id}
+                        id={account.id}
                         service={account.service}
                         imgUrl={account.logo_image}
                         userName={account.username}
+                        accounts={accounts}
+                        setAccounts={setAccounts}
                     />
                 )
             }
@@ -50,5 +118,24 @@ const styles = StyleSheet.create({
         gap: 20
         // justifyContent: 'center',
         // alignItems: 'center'
+    },
+    input:{
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#444444',
+        paddingHorizontal:10,
+        paddingVertical:6,
+        marginVertical:5,
+        borderRadius:7
+    },
+    button: ({pressed}) => [{
+        backgroundColor: pressed?'black': '#2f0636',
+        alignItems:"center",
+        marginVertical:10,
+        borderRadius:10,
+        paddingVertical:5
+    }],
+    textCad:{
+        color:'white'
     }
 })
--- components/NewCardAccount.js
+++ components/NewCardAccount.js
@@ -1,45 +1,82 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import { Image } from 'expo-image'
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
-export default function NewCardAccount({service, userName, imgUrl}){
-    return(
+export default function NewCardAccount({ id, service, userName, imgUrl, accounts, setAccounts }) {
+
+    const handleDelete = async () => {
+        const response = await fetch(`http://localhost:3000/account/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if (response.ok) {
+            const data = await response.json()
+            console.log(data)
+            const newAccounts = accounts.filter((item) => item.id !== id)
+            setAccounts(newAccounts)
+            return
+        }
+        console.log('Erro ao deletar conta')
+        return
+    }
+
+    return (
         <View style={styles.card}>
             <Image
-            style={styles.logo}
-            source={imgUrl}
+                style={styles.logo}
+                source={imgUrl}
             />
 
             <View style={styles.content}>
                 <Text style={styles.service}>{service}</Text>
                 <Text style={styles.username}>{userName}</Text>
+                <Pressable onPress={handleDelete} style={styles.del}>
+                        <FontAwesome6 name="trash" size={17} color="white" />
+                        <Text style={styles.txtdel}>EXCLUIR</Text>
+                </Pressable>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    card:{
+    card: {
         borderStyle: 'solid',
-        borderColor:'#EEEEEE',
-        borderWidth: 1, 
-        borderRadius:30
+        borderColor: '#EEEEEE',
+        borderWidth: 1,
+        borderRadius: 30
     },
-    logo:{
-        width:'100%',
-        height:150,
-        borderTopLeftRadius:30,
-        borderTopRightRadius:30
+    logo: {
+        width: '100%',
+        height: 150,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30
     },
-    content:{
-        gap:15,
-        alignItems:'center'
+    content: {
+        gap: 15,
+        alignItems: 'center'
     },
-    service:{
+    service: {
         fontSize: 25,
-        fontWeight:'bold'
+        fontWeight: 'bold'
     },
-    username:{
+    username: {
         color: 'white',
-        fontSize:15
+        fontSize: 15
+    },
+    del:{
+        flexDirection:'row',
+        padding:5,
+        backgroundColor:'red',
+        borderRadius:5,
+        borderWidth:1,
+        marginBottom:10,
+        gap:5
+    },
+    txtdel:{
+        fontWeight:'bold',
+        color:'white'
     }
 })
